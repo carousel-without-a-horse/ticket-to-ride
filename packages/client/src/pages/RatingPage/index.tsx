f46b4de64fcd67cdc1231fc63400a589bb74d811
Dmitrii Surovyev <dmitriysurovrv@gmail.com>
2023-06-01 18:22:29 +0300

    CAR-20 improvements after review

--- packages/client/src/pages/RatingPage/index.tsx
+++ packages/client/src/pages/RatingPage/index.tsx
@@ -100,25 +100,23 @@ const dataSource = [
 
 const RatingPage = () => {
   return (
-    <>
-      <Content>
-        <Card
-          title="Рейтинг Игроков"
-          style={{
-            marginBottom: 50,
-            width: '100%',
-            maxWidth: 1024,
-            height: 'calc(100vh - (64px + 50px * 2))',
-          }}>
-          <Table
-            dataSource={dataSource}
-            columns={columns}
-            pagination={false}
-            style={{ width: '100%', maxWidth: 1024 }}
-          />
-        </Card>
-      </Content>
-    </>
+    <Content>
+      <Card
+        title="Рейтинг Игроков"
+        style={{
+          marginBottom: 50,
+          width: '100%',
+          maxWidth: 1024,
+          height: 'calc(100vh - (64px + 50px * 2))',
+        }}>
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          pagination={false}
+          style={{ width: '100%', maxWidth: 1024 }}
+        />
+      </Card>
+    </Content>
   )
 }
 
