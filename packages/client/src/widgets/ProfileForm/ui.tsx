9edf3be3a559c6dbd5d181504f1b795e3a67c6ab
marilisok <mari.lisok@mail.ru>
2023-06-07 02:45:14 +0300

    profile page

--- packages/client/src/widgets/ProfileForm/ui.tsx
+++ packages/client/src/widgets/ProfileForm/ui.tsx
@@ -5,6 +5,9 @@ import { user } from './data'
 import type { ChangeEvent } from 'react'
 import { useState } from 'react'
 import userServices from '@/shared/services/userServices'
+import type { TUpload, TUploadFile } from '@/shared/ui/Upload'
+import { Upload } from '@/shared/ui/Upload'
+import { Space } from '@/shared/ui/Space'
 
 const ProfileForm = () => {
   const navigate = useNavigate()
@@ -31,55 +34,85 @@ const ProfileForm = () => {
       .catch(console.error)
   }
 
+  const [fileList, setFileList] = useState<TUploadFile[]>([])
+
+  const handleChange: TUpload['onChange'] = ({ fileList }) => {
+    setFileList(fileList)
+  }
+
   return (
     <Form layout="vertical" style={{ minWidth: '30vw' }} onFinish={onFinish}>
-      <FormInput
-        initialValue={form.firstName}
-        onChange={e => onUpdateField(e)}
-        label="Имя"
-        name="firstName"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваше имя' }]}
-      />
-      <FormInput
-        initialValue={form.secondName}
-        onChange={e => onUpdateField(e)}
-        label="Фамилия"
-        name="secondName"
-      />
-      <FormInput
-        initialValue={form.login}
-        onChange={e => onUpdateField(e)}
-        label="Логин"
-        name="login"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваш логин' }]}
-      />
-      <FormInput
-        initialValue={form.email}
-        onChange={e => onUpdateField(e)}
-        label="Почта"
-        name="email"
-        type="email"
-        rules={[
-          {
-            required: true,
-            message: 'Пожалуйста, введите вашу электронную почту',
-          },
-        ]}
-      />
-      <FormInput
-        initialValue={form.phone}
-        onChange={e => onUpdateField(e)}
-        label="Телефон"
-        name="phone"
-        type="tel"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваш телефон' }]}
-      />
-      <Button type="primary" htmlType="submit">
-        Сохранить
-      </Button>
-      <Button type="link" onClick={() => navigate(-1)}>
-        &lt; Назад
-      </Button>
+      <Space direction="horizontal">
+        <div>
+          <FormInput
+            initialValue={form.firstName}
+            onChange={e => onUpdateField(e)}
+            label="Имя"
+            name="firstName"
+            rules={[
+              { required: true, message: 'Пожалуйста, введите ваше имя' },
+            ]}
+          />
+          <FormInput
+            initialValue={form.secondName}
+            onChange={e => onUpdateField(e)}
+            label="Фамилия"
+            name="secondName"
+          />
+          <FormInput
+            initialValue={form.login}
+            onChange={e => onUpdateField(e)}
+            label="Логин"
+            name="login"
+            rules={[
+              { required: true, message: 'Пожалуйста, введите ваш логин' },
+            ]}
+          />
+          <FormInput
+            initialValue={form.email}
+            onChange={e => onUpdateField(e)}
+            label="Почта"
+            name="email"
+            type="email"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите вашу электронную почту',
+              },
+            ]}
+          />
+          <FormInput
+            initialValue={form.phone}
+            onChange={e => onUpdateField(e)}
+            label="Телефон"
+            name="phone"
+            type="tel"
+            rules={[
+              { required: true, message: 'Пожалуйста, введите ваш телефон' },
+            ]}
+          />
+        </div>
+        <div>
+          <Upload
+            name="avatar"
+            listType="picture-card"
+            accept={'image/png, image/jpeg'}
+            fileList={fileList}
+            onChange={handleChange}
+            maxCount={1}
+          >
+            {fileList.length < 1 && '+ Upload'}
+          </Upload>
+        </div>
+      </Space>
+      <div>
+        <Button type="primary" htmlType="submit">
+          Сохранить
+        </Button>
+        <Button type="link" onClick={() => navigate(-1)}>
+          &lt; Назад
+        </Button>
+      </div>
     </Form>
   )
 }
