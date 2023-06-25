c43df965614aff4b70562b77891fe2837a6a29d2
marilisok <mari.lisok@mail.ru>
2023-06-25 20:39:07 +0300

    fix

--- packages/client/src/shared/store/user/userStore.ts
+++ packages/client/src/shared/store/user/userStore.ts
@@ -5,17 +5,14 @@ import authServices from 'src/shared/services/authServices'
 import type { AxiosError } from 'axios'
 import type { TUser } from '@/pages/ProfilePage/ui/ProfileForm/type'
 
-type TStatus = 'initial' | 'loading' | 'error' | 'success'
 export class UserStore {
   user: TUser | null = null
-  status: TStatus = 'initial'
   initialized = false
   error: AxiosError | null = null
 
   constructor() {
     makeObservable(this, {
       user: observable,
-      status: observable,
       initialized: observable,
       fetchUser: action,
     })
@@ -24,17 +21,14 @@ export class UserStore {
   }
 
   async fetchUser() {
-    this.status = 'loading'
     this.error = null
     try {
       const response = await authServices.fetchUser()
       if (!response) {
         throw new Error('Empty response')
       }
-      this.status = 'success'
       this.user = response
     } catch (error) {
-      this.status = 'error'
       if ((error as AxiosError).isAxiosError) {
         this.error = error as AxiosError
       }
